e8c183d1295c0e3a6efaa4566fd6c80d9dae08b4
hardy6413 <irreftw@gmail.com>
2022-03-15 15:10:29 +0100

    prepared Farm and Field models for bidirectional relation

--- src/main/java/com/fms/models/Farm.java
+++ src/main/java/com/fms/models/Farm.java
@@ -1,9 +1,12 @@
 package com.fms.models;
 
+import java.util.Set;
+
 public class Farm {
     private Long id;
     private String farmName;
     private String farmToken;
     private String image;
     private Address address;
+    private Set<Field> fields;
 }
