faf2b50df5b379f5a0cc69a3f1dd546193f56e0d
hardy6413 <irreftw@gmail.com>
2022-03-15 13:59:22 +0100

    created Address model

---
+++ src/main/java/com/fms/models/Address.java
@@ -0,0 +1,9 @@
+package com.fms.models;
+
+public class Address {
+    private Long id;
+    private String city;
+    private String street;
+    private String postalCode;
+    private String buildingNumber;
+}
