09275c631338c50872d116a8102bc10314fadad4
hardy6413 <irreftw@gmail.com>
2022-03-16 10:56:09 +0100

    created simple entity annotations and lombok annotations

--- src/main/java/com/fms/models/UserAccount.java
+++ src/main/java/com/fms/models/UserAccount.java
@@ -1,8 +1,38 @@
 package com.fms.models;
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String email;
     private String password;
+
+
     private PersonalData personalData;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        UserAccount that = (UserAccount) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
