c54fa7e45011babc15b522f172d06571d8dea3a2
hardy6413 <irreftw@gmail.com>
2022-03-25 13:50:41 +0100

    created simple processAccountCreationForm method in login controller

--- src/main/java/com/fms/models/UserAccount.java
+++ src/main/java/com/fms/models/UserAccount.java
@@ -19,7 +19,7 @@ public class UserAccount {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(unique = true)
+    @Column(nullable = false, unique = true)
     private String email;
 
     private String password;
